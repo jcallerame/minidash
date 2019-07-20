2c811fb9a0db203c7f60e6ff13e2020547bf1e2b
John Callerame <john.callerame@gmail.com>
2019-07-20 18:48:12 -0400

    Added debounce and throttle, copied from lodash with only minor edits

--- src/index.js
+++ src/index.js
@@ -142,19 +142,7 @@ export const sumBy = (list, iteratee = identity) => {
 	let result = 0;
 	for (const item of list) {
 		const itemResult = func(item);
-		const typeTag = getTag(itemResult);
-		let numItemResult;
-		switch (typeTag) {
-			case numberTag:
-			case booleanTag:
-				numItemResult = itemResult;
-				break;
-			case stringTag:
-				numItemResult = parseInt(itemResult, 10);
-				break;
-			default:
-				numItemResult = NaN;
-		}
+		const numItemResult = +itemResult;
 		result += numItemResult;
 	}
 	return result;
@@ -782,6 +770,143 @@ export const once = func => {
 		return result;
 	};
 };
+
+// This code taken from lodash with only minor changes applied.
+export const debounce = (func, wait, options = {}) => {
+  let lastArgs,
+      lastThis,
+      maxWait,
+      result,
+      timerId,
+      lastCallTime,
+      lastInvokeTime = 0;
+
+  if (!isFunction(func)) {
+    throw new TypeError('First argument to debounce must be a function.');
+  }
+  wait = +wait || 0;
+	let leading = !!options.leading;
+	let maxing = 'maxWait' in options;
+	if (maxing) {
+		maxWait = Math.max(+options.maxWait || 0, wait);
+	}
+	let trailing = 'trailing' in options ? !!options.trailing : true;
+
+  function invokeFunc(time) {
+    let args = lastArgs,
+        thisArg = lastThis;
+
+    lastArgs = lastThis = undefined;
+    lastInvokeTime = time;
+    result = func.apply(thisArg, args);
+    return result;
+  }
+
+  function leadingEdge(time) {
+    // Reset any `maxWait` timer.
+    lastInvokeTime = time;
+    // Start the timer for the trailing edge.
+    timerId = setTimeout(timerExpired, wait);
+    // Invoke the leading edge.
+    return leading ? invokeFunc(time) : result;
+  }
+
+  function remainingWait(time) {
+    var timeSinceLastCall = time - lastCallTime,
+        timeSinceLastInvoke = time - lastInvokeTime,
+        timeWaiting = wait - timeSinceLastCall;
+
+    return maxing
+      ? Math.min(timeWaiting, maxWait - timeSinceLastInvoke)
+      : timeWaiting;
+  }
+
+  function shouldInvoke(time) {
+    let timeSinceLastCall = time - lastCallTime,
+        timeSinceLastInvoke = time - lastInvokeTime;
+
+    // Either this is the first call, activity has stopped and we're at the
+    // trailing edge, the system time has gone backwards and we're treating
+    // it as the trailing edge, or we've hit the `maxWait` limit.
+    return (lastCallTime === undefined || (timeSinceLastCall >= wait) ||
+      (timeSinceLastCall < 0) || (maxing && timeSinceLastInvoke >= maxWait));
+  }
+
+  function timerExpired() {
+    let now = Date.now();
+    if (shouldInvoke(now)) {
+      return trailingEdge(now);
+    }
+    // Restart the timer.
+    timerId = setTimeout(timerExpired, remainingWait(now));
+  }
+
+  function trailingEdge(time) {
+    timerId = undefined;
+
+    // Only invoke if we have `lastArgs` which means `func` has been
+    // debounced at least once.
+    if (trailing && lastArgs) {
+      return invokeFunc(time);
+    }
+    lastArgs = lastThis = undefined;
+    return result;
+  }
+
+  function cancel() {
+    if (timerId !== undefined) {
+      clearTimeout(timerId);
+    }
+    lastInvokeTime = 0;
+    lastArgs = lastCallTime = lastThis = timerId = undefined;
+  }
+
+  function flush() {
+    return timerId === undefined ? result : trailingEdge(Date.now());
+  }
+
+  function debounced() {
+    let now = Date.now(),
+        isInvoking = shouldInvoke(now);
+
+    lastArgs = arguments;
+    lastThis = this;
+    lastCallTime = now;
+
+    if (isInvoking) {
+      if (timerId === undefined) {
+        return leadingEdge(lastCallTime);
+      }
+      if (maxing) {
+        // Handle invocations in a tight loop.
+        timerId = setTimeout(timerExpired, wait);
+        return invokeFunc(lastCallTime);
+      }
+    }
+    if (timerId === undefined) {
+      timerId = setTimeout(timerExpired, wait);
+    }
+    return result;
+  }
+  debounced.cancel = cancel;
+  debounced.flush = flush;
+  return debounced;
+};
+
+// This function taken from lodash with only minor changes applied.
+export const throttle = (func, wait, options = {}) => {
+  if (typeof func != 'function') {
+    throw new TypeError('First argument to throttle must be a function.');
+  }
+	const leading = 'leading' in options ? !!options.leading : true;
+  const trailing = 'trailing' in options ? !!options.trailing : true;
+  return debounce(func, wait, {
+		leading,
+		trailing,
+    maxWait: wait,
+  });
+};
+
 
 //TODO:
 //pluck
